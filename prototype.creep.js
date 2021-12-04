ca5ae46824551c1e16707ed5cf7b9132f341ebeb
Denloob <loobdennis@gmail.com>
2021-12-04 16:47:21 +0200

    Redesigned getEnergy

--- prototype.creep.js
+++ prototype.creep.js
@@ -27,51 +27,46 @@ Creep.prototype.runRole =
     @param {bool} useContainer
     @param {bool} useSource */
 Creep.prototype.getEnergy =
-    function (useContainer, useSource, useOther=false) {
+    function (useContainer, useSource, useOther) {
         if (useOther) {
-            // find closest dropped resource, tombstone and ruin, with energy
-            var droppedResources = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: dr => dr.resourceType == RESOURCE_ENERGY && dr.amount > 100});
-            // var droppedResources = undefined;
-            var tombstone = this.pos.findClosestByPath(FIND_TOMBSTONES, { filter: (t) => t.store[RESOURCE_ENERGY] > 0 });
-            var ruin = this.pos.findClosestByPath(FIND_RUINS, { filter: (r) => r.store[RESOURCE_ENERGY] > 0 });
+            let droppedResources = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: dr => dr.resourceType == RESOURCE_ENERGY && dr.amount > 100});
             if(droppedResources != undefined) {
                 // try to withdraw droppedResources, if it is not in range
                 if(this.pickup(droppedResources) == ERR_NOT_IN_RANGE) {
                     // move towards it
                     this.moveTo(droppedResources, {visualizePathStyle: {stroke: '#feeb75'}});
                 }
+                return;
             }
-            else if (tombstone != undefined) {
+            let tombstone = this.pos.findClosestByPath(FIND_TOMBSTONES, { filter: (t) => t.store[RESOURCE_ENERGY] > 0 });
+            if (tombstone != undefined) {
                 // try to withdraw energy from tombstone, if it is not in range
                 if(this.withdraw(tombstone, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
                     this.moveTo(tombstone, {visualizePathStyle: {stroke: '#feeb75'}});
                 }
+                return;
             }
-            else if (ruin != undefined) {
+            let ruin = this.pos.findClosestByPath(FIND_RUINS, { filter: (r) => r.store[RESOURCE_ENERGY] > 0 });
+            if (ruin != undefined) {
                 // try to withdraw energy from ruin, if it is not in range
                 if(this.withdraw(ruin, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
                     this.moveTo(ruin, {visualizePathStyle: {stroke: '#feeb75'}});
                 }
+                return
             }
         }
-
-        /** @type {StructureContainer} */
-        let container;
         // if the Creep should look for containers
-        if (useContainer && droppedResources == undefined && tombstone == undefined && ruin == undefined) {
-            // find closest terminal
-            container = this.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => s.structureType == STRUCTURE_TERMINAL && s.store[RESOURCE_ENERGY] > 0
+        if (useContainer) {
+            //// find closest terminal
+            //// container = this.pos.findClosestByPath(FIND_STRUCTURES, {
+            ////     filter: s => s.structureType == STRUCTURE_TERMINAL && s.store[RESOURCE_ENERGY] > 0
+            //// });
+            // find closest storage
+            let container = this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 10000
             });
-            // if no terminal found
-            if (container == undefined) {
-                // find closest storage
-                container = this.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: s => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 10000
-                });
-            }
 
             // if no storage found      
             if (container == undefined) {
@@ -87,12 +82,13 @@ Creep.prototype.getEnergy =
                     // move towards it
                     this.moveTo(container, {visualizePathStyle: {stroke: '#feeb75'}});
                 }
+                return;
             }
         }
-        // if no container was found and the Creep should look for Sources
-        if (container == undefined && useSource) {
+
+        if (useSource) {
             // find closest source
-            var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {filter: s => s.id != '5bbcad679099fc012e637357s'});
+            let source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {filter: s => s.id != '5bbcad679099fc012e637357s'});
 
             // try to harvest energy, if the source is not in range
             if (this.harvest(source) == ERR_NOT_IN_RANGE) {
