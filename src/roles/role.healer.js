738d40ef1b8df6763ad136632fe262a38c08be3e
Denloob <loobdennis@gmail.com>
2022-01-06 16:24:46 +0200

    Set ignoreCreeps to true

--- src/roles/role.healer.js
+++ src/roles/role.healer.js
@@ -8,7 +8,7 @@ module.exports = {
       let mainTarget = Game.creeps[creep.memory.target];
       if (creep.room.name == mainTarget.room.name) {
         if (Math.abs(mainTarget.pos.x - creep.pos.x) > 1 || Math.abs(mainTarget.pos.y - creep.pos.y) > 1)
-          creep.moveTo(Game.creeps[creep.memory.target], { visualizePathStyle: { stroke: '#00ff00' }, range: 0 });
+          creep.moveTo(Game.creeps[creep.memory.target], { range: 0, ignoreCreeps: true, visualizePathStyle: { stroke: '#00ff00' } });
         let target = creep.pos.findClosestByRange(FIND_MY_CREEPS, { filter: (c) => c.hits < c.hitsMax });
         if (target) {
           creep.heal(target);
