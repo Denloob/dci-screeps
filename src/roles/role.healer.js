5bcd02b429ae8f6089d641fa0feac75fecc0cf4b
Denloob <loobdennis@gmail.com>
2022-01-06 15:46:03 +0200

    Changed the range target  is considered reached

--- src/roles/role.healer.js
+++ src/roles/role.healer.js
@@ -1,20 +1,19 @@
 module.exports = {
-    // a function to run the logic for this role
-    run: function(creep) {
-        if (Game.creeps[creep.memory.target] != undefined) {
-            let mainTarget = Game.creeps[creep.memory.target];
-            if (creep.room.name == mainTarget.room.name) {
-                if (Math.abs(mainTarget.pos.x-creep.pos.x) > 1 || Math.abs(mainTarget.pos.y-creep.pos.y) > 1)
-                    creep.moveTo(Game.creeps[creep.memory.target], {visualizePathStyle: {stroke: '#00ff00'}});
-                let target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {filter: (c) => c.hits < c.hitsMax});
-                if(target) {
-                    creep.heal(target);
-                }
-            }
-            else {
-                // travel to target creep room
-                creep.travelTo(new RoomPosition(25, 25, Game.creeps[creep.memory.target].room.name));
-            }
+  // a function to run the logic for this role
+  run: function (creep) {
+    if (Game.creeps[creep.memory.target] != undefined) {
+      let mainTarget = Game.creeps[creep.memory.target];
+      if (creep.room.name == mainTarget.room.name) {
+        if (Math.abs(mainTarget.pos.x - creep.pos.x) > 1 || Math.abs(mainTarget.pos.y - creep.pos.y) > 1)
+          creep.moveTo(Game.creeps[creep.memory.target], { visualizePathStyle: { stroke: '#00ff00' }, range: 0 });
+        let target = creep.pos.findClosestByRange(FIND_MY_CREEPS, { filter: (c) => c.hits < c.hitsMax });
+        if (target) {
+          creep.heal(target);
         }
+      } else {
+        // travel to target creeps room
+        creep.travelTo(new RoomPosition(25, 25, Game.creeps[creep.memory.target].room.name));
+      }
     }
+  },
 };
