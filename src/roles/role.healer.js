e3cc148b94756f934dc8d675fcc5b3b2ce95353f
Denloob <loobdennis@gmail.com>
2022-01-06 17:53:09 +0200

    Fixed movement of healers in duos

--- src/roles/role.healer.js
+++ src/roles/role.healer.js
@@ -7,8 +7,7 @@ module.exports = {
     if (Game.creeps[creep.memory.target] != undefined) {
       let mainTarget = Game.creeps[creep.memory.target];
       if (creep.room.name == mainTarget.room.name) {
-        if (Math.abs(mainTarget.pos.x - creep.pos.x) > 1 || Math.abs(mainTarget.pos.y - creep.pos.y) > 1)
-          creep.moveTo(Game.creeps[creep.memory.target], { range: 0, ignoreCreeps: true, visualizePathStyle: { stroke: '#00ff00' } });
+        creep.moveTo(Game.creeps[creep.memory.target], { range: 0, ignoreCreeps: true, visualizePathStyle: { stroke: '#00ff00' } });
         let target = creep.pos.findClosestByRange(FIND_MY_CREEPS, { filter: (c) => c.hits < c.hitsMax });
         if (target) {
           creep.heal(target);
