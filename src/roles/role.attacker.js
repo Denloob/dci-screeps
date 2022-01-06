2e2d2023040351f649b4b0fbd5e589b543d3c6cb
Denloob <loobdennis@gmail.com>
2022-01-06 15:38:23 +0200

    Added attacking of unowned structures

--- src/roles/role.attacker.js
+++ src/roles/role.attacker.js
@@ -1,67 +1,73 @@
 module.exports = {
-    // a function to run the logic for this role
-    /**
-     * @param  {Creep} creep
-     */
-    run: function (creep) {
-        // if in target room
-        if (creep.room.name == creep.memory.target) {
-            // find closest enemy creep
-            let target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, { filter: (c) => _.filter(c.body, b => b.type == ATTACK || b.type == HEAL).length });
-            if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_INVADER_CORE });
-            if (target == undefined) {
-                target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
-                    filter: (s) => s.structureType == STRUCTURE_TOWER &&
-                        _.filter(creep.room.lookAt(s.pos), o => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART)[0] == undefined
-                });
-                let maxRampartHits = 50000;
-                let itr = 10000;
-                if (target == undefined) for (let i = 0; i < maxRampartHits && target == undefined; i += itr) {
-                    target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
-                        filter: (s) => s.structureType == STRUCTURE_TOWER && (
-                            _.filter(creep.room.lookAt(s.pos), o => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART && o.structure.hits < i)[0])
-                    });
-                }
-            }
-            if (target == undefined) {
-                target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
-                    filter: (s) => s.structureType == STRUCTURE_SPAWN &&
-                        _.filter(creep.room.lookAt(s.pos), o => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART)[0] == undefined
-                });
-                let maxRampartHits = 100000;
-                let itr = 10000;
-                if (target == undefined) for (let i = 0; i < maxRampartHits && target == undefined; i += itr) {
-                    target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
-                        filter: (s) => s.structureType == STRUCTURE_SPAWN && (
-                            _.filter(creep.room.lookAt(s.pos), o => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART && o.structure.hits < i)[0])
-                    });
-                }
-                if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-                if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType != STRUCTURE_CONTROLLER && s.hits < 30000 });
-                if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType != STRUCTURE_CONTROLLER });
-            }
-            // if one is found
-            if (target != undefined) {
-                // try to attack
-                let attack = creep.attack(target)
-                // if the enemy is not in range
-                if (attack == ERR_NOT_IN_RANGE) {
-                    // move towards the enemy
-                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ff0000' } });
-                }
-                else if (attack == OK) console.log(`${creep.name} is attacking ${target} in ${creep.room}, hp left: ${creep.hits} while targets hits are ${target.hits}`)
-            }
-            if (creep.pos.y == 0) creep.move(BOTTOM);
-            else if (creep.pos.y == 49) creep.move(TOP);
-            else if (creep.pos.x == 0) creep.move(RIGHT);
-            else if (creep.pos.x == 49) creep.move(LEFT);
-        }
-        // if not in target room and not waiting...
-        else if (!creep.memory.waiting) {
-            // travel to target room
-            creep.travelTo(new RoomPosition(25, 25, creep.memory.target));
-        }
+  // a function to run the logic for this role
+  /**
+   * @param  {Creep} creep
+   */
+  run: function (creep) {
+    // if in target room
+    if (creep.room.name == creep.memory.target) {
+      // find closest enemy creep
+      let target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, { filter: (c) => _.filter(c.body, (b) => b.type == ATTACK || b.type == HEAL).length });
+      if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_INVADER_CORE });
+      if (target == undefined) {
+        target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+          filter: (s) =>
+            s.structureType == STRUCTURE_TOWER &&
+            _.filter(creep.room.lookAt(s.pos), (o) => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART)[0] == undefined,
+        });
+        let maxRampartHits = 50000;
+        let itr = 10000;
+        if (target == undefined)
+          for (let i = 0; i < maxRampartHits && target == undefined; i += itr) {
+            target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+              filter: (s) =>
+                s.structureType == STRUCTURE_TOWER &&
+                _.filter(creep.room.lookAt(s.pos), (o) => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART && o.structure.hits < i)[0],
+            });
+          }
+      }
+      if (target == undefined) {
+        target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+          filter: (s) =>
+            s.structureType == STRUCTURE_SPAWN &&
+            _.filter(creep.room.lookAt(s.pos), (o) => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART)[0] == undefined,
+        });
+        let maxRampartHits = 100000;
+        let itr = 10000;
+        if (target == undefined)
+          for (let i = 0; i < maxRampartHits && target == undefined; i += itr) {
+            target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+              filter: (s) =>
+                s.structureType == STRUCTURE_SPAWN &&
+                _.filter(creep.room.lookAt(s.pos), (o) => o.type == 'structure' && o.structure.structureType == STRUCTURE_RAMPART && o.structure.hits < i)[0],
+            });
+          }
+      }
+      if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+      if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType != STRUCTURE_CONTROLLER && s.hits < 30000 });
+      if (target == undefined) target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType != STRUCTURE_CONTROLLER });
+      if (target == undefined) target = creep.pos.findClosestByPath(FIND_STRUCTURES);
+      // if one is found
+      if (target != undefined) {
+        // try to attack
+        let attack = creep.attack(target);
+        // if the enemy is not in range
+        if (attack == ERR_NOT_IN_RANGE) {
+          // move towards the enemy
+          creep.moveTo(target, { visualizePathStyle: { stroke: '#ff0000' } });
+        } else if (attack == OK) console.log(`${creep.name} is attacking ${target} in ${creep.room}, hp left: ${creep.hits} while targets hits are ${target.hits}`);
+      }
+      if (creep.pos.y == 0) creep.move(BOTTOM);
+      else if (creep.pos.y == 49) creep.move(TOP);
+      else if (creep.pos.x == 0) creep.move(RIGHT);
+      else if (creep.pos.x == 49) creep.move(LEFT);
     }
+    // if not in target room and not waiting...
+    else if (!creep.memory.waiting) {
+      // travel to target room
+      creep.travelTo(new RoomPosition(25, 25, creep.memory.target));
+    }
+  },
 };
 // let room = Game.rooms['#{room}'];
 // if (room.controller && room.controller.my) {
@@ -115,7 +121,6 @@ module.exports = {
 //         this.memory.moving = path[0].direction;
 //         return this.move(path[0].direction);
 //     }
-
 
 //     // put me to role.attacker.js
 //     let dir = Game.creeps[target].memory.dir;
